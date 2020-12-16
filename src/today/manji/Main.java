02dd290be81fb3dca48dd2eaf6f11052314fad91
Jay-Home <a1575208138@gmail.com>
2020-12-16 22:24:30 +0800

    Work with linked list

--- src/today/manji/Main.java
+++ src/today/manji/Main.java
@@ -1,13 +1,21 @@
 package today.manji;
 
-import today.manji.arrays.Basics;
-import today.manji.arrays.DynamicArrays;
-import today.manji.arrays.MyArray;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        DynamicArrays.main();
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        System.out.println(list);
+        System.out.println(list.contains(10));
+        System.out.println(list.indexOf(10));
+        System.out.println(list.size());
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
