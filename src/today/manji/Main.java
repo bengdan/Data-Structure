053588de6c22712d92a8462ea546f03df8b1bcf5
manji <a1575208138@gmail.com>
2021-04-25 23:20:32 +0800

    Stack: stack in java && reverse string

--- src/today/manji/Main.java
+++ src/today/manji/Main.java
@@ -4,14 +4,36 @@ import today.manji.linkedList.LinkedList;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-       var list = new LinkedList();
-       list.addLast(1);
-       list.addLast(2);
-       list.addLast(3);
+        var stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack);
+
+        System.out.println("------Reverse String--------");
+
+        System.out.println(reverseString("Manji"));
+    }
+
+    public static String reverseString(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+        var stack = new Stack<Character>();
+        for (char c:
+        str.toCharArray())
+            stack.push(c);
+
+        StringBuffer output = new StringBuffer();
+        while (!stack.isEmpty()) output.append(stack.pop());
+        return output.toString();
     }
 }
